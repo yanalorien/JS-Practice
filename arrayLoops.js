b5cbb9b94e74ed183dfa52ec05550da5bb86f945
yanalorien <yankaeva@gmail.com>
2020-10-25 20:25:08 -0400

    added tasks from codewars, loops in array

--- arrayLoops.js
+++ arrayLoops.js
@@ -152,3 +152,92 @@ for (let i = 0; i < arr.length; i++) {
     }
 }
 console.log(arr);
+
+//Assume "#" is like a backspace in string.
+//This means that string "a#bc#d" actually is "bd"
+//Your task is to process a string with "#" symbols.
+//Examples:  "abc#d##c"      ==>  "ac"
+function cleanString(s) {
+    const result = []
+    for (const c of s) {
+        if (c === "#") {
+            result.pop()
+        } else {
+            result.push(c)
+        }
+    }
+    return result.join("")
+};
+
+
+
+
+function sumOfFractionals(n){
+    let sum = 1;
+    for (let i = 2; i <= n; i++) {
+        sum += 1/i;
+    }
+    return +sum.toFixed(2);
+}
+//Дано число n > 0. Найдите сумму: 1 + 1/2 + 1/3 + 1/4 + ... + 1/n
+//Напишите функцию с именем sumOfFractionals, которая принимает число n
+//и возвращает сумму дробей, у которых в числителе 1, а в знаменателе - числа от 1 до n.
+//В решении используйте цикл for. Сумму округлите до двух десятичнных знаков.
+
+
+
+//Your task is to write a function which returns the sum of following series upto nth term(parameter).
+//Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+//Rules: You need to round the answer to 2 decimal places and return it as String.
+//If the given value is 0 then it should return 0.00
+//You will only be given Natural Numbers as arguments.
+
+function SeriesSum(n){
+    let sum = 0;
+    let x = 1;
+    for (let i = 0; i < n; i++){
+        sum += 1 / x;
+        x += 3;
+    }
+    return sum.toFixed(2);
+}
+
+//Find the sum of all multiples of n below m
+//Keep in Mind: n and m are natural numbers (positive integers)
+//m is excluded from the multiples
+// 1 способ
+function sumMul(n,m){
+    let sum = 0;
+    if (m < n || m === 0) return 'INVALID';
+    for (let i = 1; i * n < m; i++){
+        sum += i * n;
+    }
+    return sum;
+}
+//2 способ
+function sumMul(n,m){
+    if (n >= m) return "INVALID";
+
+    var sum = 0;
+    for (var i = n; i < m; i+=n) {
+        sum += i;
+    }
+    return sum;
+}
+
+
+// Given a number n, draw stairs using the letter "I",
+//n tall and n wide, with the tallest in the top left.
+//For example n = 3 result in: "I\n I\n  I"
+function drawStairs(n) {
+    let str = '';
+    let x = ' ';
+    for (let i = 1; i <= n; i++) {
+        if (i === n) return str += 'I';
+        str += "I\n";
+        for (let y = 1; y <= i; y++) {
+            str += x;
+        }
+    }
+    return str;
+}
